0511d81f1177a07299a8a723f7c8a3599e8e3371
kkpandey98 <kkpandey1998@gmail.com>
2020-11-23 19:51:37 +0530

    First Commit

---
+++ html_css/img_name_mouseover/js/javascript.js
@@ -0,0 +1,9 @@
+function change(x){
+    document.getElementById('f01').innerHTML=x.alt
+    
+}
+
+function normal(){
+    document.getElementById('f01').innerHTML = "Hover Here To Change"
+    
+}
